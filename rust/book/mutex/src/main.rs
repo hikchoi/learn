0c557b98b8c3ae0739fccbb29f93b5b442cecc7b
Mark Choi <hikchoi@gmail.com>
2023-05-07 10:36:35 +0900

    update

--- rust/book/mutex/src/main.rs
+++ rust/book/mutex/src/main.rs
@@ -1,3 +1,38 @@
+use std::sync::{Mutex, Arc};
+use std::thread;
+
+fn _one() {
+    let m = Mutex::new(5);
+    
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+    
+    println!("m = {:?}", m);
+}
+
+fn _two() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
 fn main() {
-    println!("Hello, world!");
+    _two();
 }
